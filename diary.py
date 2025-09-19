0833fd1b413af908888bbbb8860c7b055a4d6a59
Tife <boluwatife1904@gmail.com>
2025-09-19 01:16:46 +0100

    organised merge for search by keyword fn

--- diary.py
+++ diary.py
@@ -1,3 +1,6 @@
+import re
+
+
 entries = {
   "entries": [
     {
@@ -97,5 +100,25 @@ class Diary():
             return results
         except No_Result_Error as e:
             print(e)
+
+    def search_by_keyword(text, keyword):
+      pattern = re.compile(rf"\b{re.escape(keyword)}\b", re.IGNORECASE)
+      matches = pattern.findall(text)
+      return matches
+
+
+
+
+# # Example usage
+# text = "The quick brown fox jumps over the lazy dog."
+# keyword = "fox"
+# matches = search_by_keyword(text, keyword)
+
+# if matches:
+#     print(f"Found '{keyword}' in the text.")
+# else:
+#     print(f"'{keyword}' not found in the text.")
+
+
 
 
